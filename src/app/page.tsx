c6b3f9ce7455986d86038d99aa8781e9a3cf9521
oo11boy <editkhone@gmail.com>
2025-03-28 04:11:24 +0430

    fix:add genres

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/Components/Header/Header";
 import MovieSlider from "@/Components/MovieSlider/MovieSlider";
 import MovieSliderCount from "@/Components/MovieSliderCount/MovieSliderCount";
+import SelectGenre from "@/Components/SelectGenres/SelectGenre";
 
 export default function page() {
   return <>
    <Header/>
 <MovieSlider/>
+<SelectGenre/>
 <MovieSliderCount/>
+
   </>
 }
